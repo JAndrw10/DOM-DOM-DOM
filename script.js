bdc9c78f2ee4e22984c1b93654c1ce271fffe776
JosephOlvr <77339938+JAndrw10@users.noreply.github.com>
2021-01-29 12:55:45 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    
+    let btn = document.createElement("Button");
+    document.body.appendChild(btn);
+    btn.textContent = "Add Square";
+});
+
+    let container = document.createElement("div");
+    container.className = "Square";
+
+    
